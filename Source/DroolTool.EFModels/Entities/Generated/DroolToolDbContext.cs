84bc358aa9da7bf80fcce6d7ed6a6d18cd1b7bf2
raynardlee <raynardlee@yahoo.com>
2020-03-13 13:10:10 -0700

    initial commit that has a functioning api and web

---
+++ Source/DroolTool.EFModels/Entities/Generated/DroolToolDbContext.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DroolTool.EFModels.Entities
+{
+    public partial class DroolToolDbContext : DbContext
+    {
+        public DroolToolDbContext()
+        {
+        }
+
+        public DroolToolDbContext(DbContextOptions<DroolToolDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DatabaseMigration> DatabaseMigration { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            {
+
+                
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DatabaseMigration>(entity =>
+            {
+                entity.HasKey(e => e.DatabaseMigrationNumber)
+                    .HasName("PK_DatabaseMigration_DatabaseMigrationNumber");
+
+                entity.Property(e => e.DatabaseMigrationNumber).ValueGeneratedNever();
+            });
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.HasIndex(e => e.RoleDisplayName)
+                    .HasName("AK_Role_RoleDisplayName")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.RoleName)
+                    .HasName("AK_Role_RoleName")
+                    .IsUnique();
+
+                entity.Property(e => e.RoleID).ValueGeneratedNever();
+
+                entity.Property(e => e.RoleDescription).IsUnicode(false);
+
+                entity.Property(e => e.RoleDisplayName).IsUnicode(false);
+
+                entity.Property(e => e.RoleName).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Email)
+                    .HasName("AK_User_Email")
+                    .IsUnique();
+
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.LoginName).IsUnicode(false);
+
+                entity.Property(e => e.Phone).IsUnicode(false);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.User)
+                    .HasForeignKey(d => d.RoleID)
+                    .OnDelete(DeleteBehavior.ClientSetNull);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
