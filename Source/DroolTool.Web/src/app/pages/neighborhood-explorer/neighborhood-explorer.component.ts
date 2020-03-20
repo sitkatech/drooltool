933205a44cb40325402eca65f1f4d49c3b6a12b5
Mack Peters <mack.peters@sitkatech.com>
2020-03-19 17:12:40 -0700

    Added overlay layers and required packages to bring in esri dynamic layers as well as invert colors for the mask

--- Source/DroolTool.Web/src/app/pages/neighborhood-explorer/neighborhood-explorer.component.ts
+++ Source/DroolTool.Web/src/app/pages/neighborhood-explorer/neighborhood-explorer.component.ts
@@ -1,7 +1,11 @@
 import { AfterViewInit, ApplicationRef, ChangeDetectionStrategy, Component, EventEmitter, OnInit} from '@angular/core';
+import { environment } from "src/environments/environment";
 import * as L from 'leaflet';
+import '../../../../node_modules/leaflet.snogylop/src/leaflet.snogylop.js';
+import * as esri from 'esri-leaflet'
 import { BoundingBoxDto } from '../../shared/models/bounding-box-dto';
 import { CustomCompileService } from '../../shared/services/custom-compile.service';
+import { NeighborhoodExplorerService } from 'src/app/services/neighborhood-explorer/neighborhood-explorer.service'
 
 @Component({
   selector: 'drooltool-neighborhood-explorer',
@@ -25,22 +29,25 @@ export class NeighborhoodExplorerComponent implements OnInit {
     public layerControl: L.Control.Layers;
     public tileLayers: { [key: string]: any } = {};
     public overlayLayers: { [key: string]: any } = {};
+    public maskLayer: any;
     
     boundingBox: BoundingBoxDto;
 
     constructor(
         private appRef: ApplicationRef,
-        private compileService: CustomCompileService
+        private compileService: CustomCompileService,
+        private neighborhoodExplorerService : NeighborhoodExplorerService
     ) {
     }
 
     public ngOnInit(): void {
+
         // Default bounding box
         this.boundingBox = new BoundingBoxDto();
-        this.boundingBox.Left = -119.075;
-        this.boundingBox.Bottom = 35.442022035628575;
-        this.boundingBox.Right = -119.425;
-        this.boundingBox.Top = 35.27608156273151;
+        this.boundingBox.Left = -117.36883651141554;
+        this.boundingBox.Bottom = 33.45695062823788;
+        this.boundingBox.Right = -117.82471349197476;
+        this.boundingBox.Top = 33.71689407051289;
 
         this.tileLayers = Object.assign({}, {
             "Aerial": L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
@@ -65,19 +72,67 @@ export class NeighborhoodExplorerComponent implements OnInit {
             })
         }, this.tileLayers);
 
+        let neighborhoodsWMSOptions = ({
+          layers: "OCStormwater:Neighborhoods",
+          transparent: true,
+          format: "image/png",
+          tiled: true
+        } as L.WMSOptions);
+
+        let backboneWMSOptions = ({
+          layers: "OCStormwater:Backbone",
+          transparent: true,
+          format: "image/png",
+          tiled: true
+        } as L.WMSOptions);
+
+        let watershedsWMSOptions = ({
+          layers: "OCStormwater:DroolToolWatersheds",
+          transparent: true,
+          format: "image/png",
+          tiled: true
+        } as L.WMSOptions);
+
+
+
+        this.overlayLayers = Object.assign({}, {
+          "Neighborhoods": L.tileLayer.wms(environment.geoserverMapServiceUrl + "/wms?", neighborhoodsWMSOptions),
+          "Streams": L.tileLayer.wms(environment.geoserverMapServiceUrl + "/wms?", backboneWMSOptions),
+          "Watersheds": L.tileLayer.wms(environment.geoserverMapServiceUrl + "/wms?", watershedsWMSOptions),
+          "Stormwater Network": esri.dynamicMapLayer({url:"https://ocgis.com/arcpub/rest/services/Flood/Stormwater_Network/MapServer/"})
+        })
+
         this.compileService.configure(this.appRef);
     }
 
     public ngAfterViewInit(): void {
 
-        const mapOptions: L.MapOptions = {
+      this.neighborhoodExplorerService.getMask().subscribe(maskString => {
+        this.maskLayer = L.geoJSON(maskString, {
+                                        invert: true,
+                                        style: function (feature) {
+                                            return {
+                                                fillColor: "#323232",
+                                                fill: true,
+                                                fillOpacity: 0.4,
+                                                color: "#3388ff",
+                                                weight: 5,
+                                                stroke: true
+                                            };
+                                        }
+                                    });
+
+          const mapOptions: L.MapOptions = {
             minZoom: 6,
             maxZoom: 22,
             layers: [
                 this.tileLayers["Hillshade"],
+                this.overlayLayers["Streams"],
+                this.overlayLayers["Watersheds"]
             ],
             
         } as L.MapOptions;
+
         this.map = L.map(this.mapID, mapOptions);
 
         this.map.on('load', (event: L.LeafletEvent) => {
@@ -86,11 +141,12 @@ export class NeighborhoodExplorerComponent implements OnInit {
         this.map.on("moveend", (event: L.LeafletEvent) => {
             this.onMapMoveEnd.emit(event);
         });
-        this.map.fitBounds([[this.boundingBox.Bottom, this.boundingBox.Left], [this.boundingBox.Top, this.boundingBox.Right]], this.defaultFitBoundsOptions);
-        this.map.setZoom(10);
+        this.map.fitBounds(this.maskLayer.getBounds(), this.defaultFitBoundsOptions);
+        this.map.setZoom(12);
         
         this.setControl();
-        
+        this.maskLayer.addTo(this.map);
+      });              
     }
 
     public setControl(): void {
