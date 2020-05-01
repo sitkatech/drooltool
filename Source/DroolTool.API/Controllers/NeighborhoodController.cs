19f8a1d81492bf39eb4c2c367405cb68f465f9c4
Mack Peters <mack.peters@sitkatech.com>
2020-05-01 13:16:20 -0700

    Small refactor to prevent repeated code and also be more concise

--- Source/DroolTool.API/Controllers/NeighborhoodController.cs
+++ Source/DroolTool.API/Controllers/NeighborhoodController.cs
@@ -35,32 +35,23 @@ namespace DroolTool.API.Controllers
         {
             var backboneAccumulated = new List<BackboneSegment>();
 
-            var backboneSegments = _dbContext.BackboneSegment
+            var allBackboneSegments = _dbContext.BackboneSegment
                 .Include(x => x.Neighborhood)
                 .Include(x => x.DownstreamBackboneSegment)
                 .Include(x => x.InverseDownstreamBackboneSegment)
+                .Where(x => x.BackboneSegmentTypeID != (int)BackboneSegmentTypeEnum.Channel)
                 .ToList();
 
-            var lookingAt = backboneSegments.Where(x => x.NeighborhoodID == neighborhoodID && x.BackboneSegmentTypeID != (int)BackboneSegmentTypeEnum.Channel).ToList();
+            var lookingAt = allBackboneSegments.Where(x => x.NeighborhoodID == neighborhoodID).ToList();
 
             while (lookingAt.Any())
             {
                 backboneAccumulated.AddRange(lookingAt);
 
-                var newEntities = backboneSegments.Where(x => lookingAt.Contains(x));
+                var downFromHere =
+                    GetDownstreamBackboneSegmentsBasedOnCriteria(allBackboneSegments, backboneAccumulated);
 
-                var downFromHere = newEntities.Where(x => x.DownstreamBackboneSegment != null)
-                    .Select(x => x.DownstreamBackboneSegment)
-                    .Where(x => x.BackboneSegmentTypeID != (int)BackboneSegmentTypeEnum.Channel)
-                    .ToList()
-                    .Distinct()
-                    .Except(backboneAccumulated);
-
-                var upFromHere = newEntities.SelectMany(x => x.InverseDownstreamBackboneSegment)
-                    .Where(x => x.BackboneSegmentTypeID != (int)BackboneSegmentTypeEnum.Channel)
-                    .ToList()
-                    .Distinct()
-                    .Except(backboneAccumulated);
+                var upFromHere = GetInverseDownstreamBackboneSegmentsBasedOnCriteria(allBackboneSegments, backboneAccumulated);
 
                 lookingAt = upFromHere.Union(downFromHere).ToList();
             }
@@ -85,26 +76,22 @@ namespace DroolTool.API.Controllers
         [HttpGet("neighborhood/{neighborhoodID}/get-downstream-backbone-trace")]
         public ActionResult<string> GetDownstreamBackboneTrace([FromRoute] int neighborhoodID)
         {
-            var backboneDownstream = new List<BackboneSegment>();
+            var backboneAccumulated = new List<BackboneSegment>();
 
-            var backboneSegments = _dbContext.BackboneSegment
+            var allBackboneSegments = _dbContext.BackboneSegment
                 .Include(x => x.DownstreamBackboneSegment)
                 .ToList();
 
-            var lookingAt = backboneSegments.Where(x => x.NeighborhoodID == neighborhoodID).ToList();
+            var lookingAt = allBackboneSegments.Where(x => x.NeighborhoodID == neighborhoodID).ToList();
 
             while (lookingAt.Any())
             {
-                backboneDownstream.AddRange(lookingAt);
+                backboneAccumulated.AddRange(lookingAt);
 
-                lookingAt = backboneSegments.Where(x => lookingAt.Contains(x) && x.DownstreamBackboneSegment != null)
-                    .Select(x => x.DownstreamBackboneSegment)
-                    .ToList()
-                    .Distinct()
-                    .ToList();
+                lookingAt = GetDownstreamBackboneSegmentsBasedOnCriteria(allBackboneSegments, backboneAccumulated);
             }
 
-            var featureList = backboneDownstream.Select(x =>
+            var featureList = backboneAccumulated.Select(x =>
             {
                 var geometry = UnaryUnionOp.Union(x.BackboneSegmentGeometry4326);
                 var feature = new Feature() { Geometry = geometry };
@@ -117,34 +104,30 @@ namespace DroolTool.API.Controllers
         [HttpGet("neighborhood/{neighborhoodID}/get-upstream-backbone-trace")]
         public ActionResult<string> GetUpstreamBackboneTrace([FromRoute] int neighborhoodID)
         {
-            var backboneUpstream = new List<BackboneSegment>();
+            var backboneAccumulated = new List<BackboneSegment>();
 
-            var backboneSegments = _dbContext.BackboneSegment
+            var allBackboneSegments = _dbContext.BackboneSegment
                 .Include(x => x.InverseDownstreamBackboneSegment)
                 .Include(x => x.Neighborhood)
                 .ToList();
 
-            var lookingAt = backboneSegments.Where(x => x.NeighborhoodID == neighborhoodID).ToList();
+            var lookingAt = allBackboneSegments.Where(x => x.NeighborhoodID == neighborhoodID).ToList();
 
             while (lookingAt.Any())
             {
-                backboneUpstream.AddRange(lookingAt);
+                backboneAccumulated.AddRange(lookingAt);
 
-                lookingAt = backboneSegments.Where(x => lookingAt.Contains(x) && x.InverseDownstreamBackboneSegment != null)
-                    .SelectMany(x => x.InverseDownstreamBackboneSegment)
-                    .ToList()
-                    .Distinct()
-                    .ToList();
+                lookingAt = GetInverseDownstreamBackboneSegmentsBasedOnCriteria(allBackboneSegments, backboneAccumulated);
             }
 
-            var featureList = backboneUpstream.Select(x =>
+            var featureList = backboneAccumulated.Select(x =>
             {
                 var geometry = UnaryUnionOp.Union(x.BackboneSegmentGeometry4326);
                 var feature = new Feature() { Geometry = geometry };
                 return feature;
             }).ToList();
 
-            var stormshed = backboneUpstream.Select(x => x.Neighborhood)
+            var stormshed = backboneAccumulated.Select(x => x.Neighborhood)
                 .ToList()
                 .Distinct()
                 .Where(x => x != null)
@@ -158,6 +141,25 @@ namespace DroolTool.API.Controllers
             featureList.Add(feature);
 
             return Ok(GeoJsonWriterService.buildFeatureCollectionAndWriteGeoJson(featureList));
+        }
+
+        private List<BackboneSegment> GetDownstreamBackboneSegmentsBasedOnCriteria(List<BackboneSegment> allBackboneSegments,
+            List<BackboneSegment> accumulatedBackboneSegments)
+        {
+            var backboneIDsThatMeetCriteria = allBackboneSegments.Where(accumulatedBackboneSegments.Contains)
+                .Where(x => x.DownstreamBackboneSegmentID.HasValue)
+                .Select(x => x.DownstreamBackboneSegmentID.Value)
+                .ToList();
+            return allBackboneSegments.Where(x => backboneIDsThatMeetCriteria.Contains(x.BackboneSegmentID)).Except(accumulatedBackboneSegments).Distinct().ToList();
+        }
+        private List<BackboneSegment> GetInverseDownstreamBackboneSegmentsBasedOnCriteria(List<BackboneSegment> allBackboneSegments,
+            List<BackboneSegment> accumulatedBackboneSegments)
+        {
+            var backboneIDsThatMeetCriteria = allBackboneSegments.Where(accumulatedBackboneSegments.Contains)
+                .Where(x => x.InverseDownstreamBackboneSegment != null)
+                .SelectMany(x => x.InverseDownstreamBackboneSegment.Select(y => y.BackboneSegmentID))
+                .ToList();
+            return allBackboneSegments.Where(x => backboneIDsThatMeetCriteria.Contains(x.BackboneSegmentID)).Except(accumulatedBackboneSegments).Distinct().ToList();
         }
     }
 }
