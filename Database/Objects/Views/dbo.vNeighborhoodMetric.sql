9917c61604291ed292bfb9b63f591b1c99c5efc7
Mack Peters <mack.peters@sitkatech.com>
2020-05-27 08:09:24 -0700

    Total irrigated area should be in acres, not square feet

--- Database/Objects/Views/dbo.vNeighborhoodMetric.sql
+++ Database/Objects/Views/dbo.vNeighborhoodMetric.sql
@@ -16,7 +16,7 @@ Select
 	round([overall_rebate_participation_fraction]  * 100, 2) as PercentParticipation,
 	round([overall_daily_est_outdoor_budget_overage_per_irrig_area] * 748.052 * 43560 * 30,0) as DroolPerLandscapedAcre,
 	[overall_MeterID_count] as TotalWaterAccounts,
-	[overall_irrg_area_sqft_sum] as TotalIrrigatedArea,
+	round([overall_irrg_area_sqft_sum] / 43560, 2) as TotalIrrigatedArea,
 	round([overall_est_outdoor_usage_sum] * 748.052 * 30, 0) as TotalWaterUsedForIrrigation
 from dbo.RawDroolMetric
 
