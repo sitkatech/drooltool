ad781dad6a613384b7b07bcf8ca3712291acb6eb
Mack Peters <mack.peters@sitkatech.com>
2020-06-12 15:43:18 -0700

    Bulk of logic now in. Columns for editing and deleting, all crud operations have been successful, homepage pulls posts and displays a message if they aren't available, modals don't allow exit during submitting, file types and size are verifying, table updates after crud operations, dates display properly for ngbdatepicker, all seems to be functional

--- Source/DroolTool.API/Controllers/NewsAndAnnouncementsController.cs
+++ Source/DroolTool.API/Controllers/NewsAndAnnouncementsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NetTopologySuite.Features;
 using NetTopologySuite.Operation.Union;
+using Newtonsoft.Json.Linq;
 
 namespace DroolTool.API.Controllers
 {
@@ -29,21 +31,57 @@ namespace DroolTool.API.Controllers
             return Ok(NewsAndAnnouncements.GetNewsAndAnnouncements(_dbContext));
         }
 
-        [HttpPost("news-and-announcements/upsert-news-and-announcements/{upsertDto}")]
-        public async Task<ActionResult> UpsertNewsAndAnnouncements([FromRoute] NewsAndAnnouncementsUpsertDto upsertDto)
+        [HttpGet("news-and-announcements/get-news-and-announcements-for-homepage")]
+        public ActionResult<List<NewsAndAnnouncementsDto>> GetNewsAndAnnouncementsForHomepage()
         {
-            if (upsertDto.NewsAndAnnouncementsID != null)
+            return Ok(NewsAndAnnouncements.GetNewsAndAnnouncementsByDate(_dbContext, 2));
+        }
+
+        [HttpPost("news-and-announcements/upsert-news-and-announcements/{newsAndAnnouncementsID}/{title}/{date}/{link?}")]
+        public async Task<ActionResult> UpsertNewsAndAnnouncements([FromRoute] int newsAndAnnouncementsID, [FromRoute] string title, [FromRoute] string date, [FromRoute] string link = null)
+        {
+            if (newsAndAnnouncementsID == -1)
             {
                 var fileResource = await HttpUtilities.MakeFileResourceFromHttpRequest(Request, _dbContext, HttpContext);
                 _dbContext.FileResource.Add(fileResource);
                 _dbContext.SaveChanges();
 
-                NewsAndAnnouncements.CreateNewsAndAnnouncementsEntity(_dbContext, upsertDto.Title, upsertDto.Date, upsertDto.Link, UserContext.GetUserFromHttpContext(_dbContext, HttpContext).UserID, fileResource.FileResourceID);
+                NewsAndAnnouncements.CreateNewsAndAnnouncementsEntity(_dbContext, title, Convert.ToDateTime(date), link, UserContext.GetUserFromHttpContext(_dbContext, HttpContext).UserID, fileResource.FileResourceID);
             }
             else
             {
-                
+                var fileResourceID = -1;
+                if (Request.ContentLength != 0)
+                {
+                    var fileResource = await HttpUtilities.MakeFileResourceFromHttpRequest(Request, _dbContext, HttpContext);
+                    _dbContext.FileResource.Add(fileResource);
+                    _dbContext.SaveChanges();
+                    fileResourceID = fileResource.FileResourceID;
+                }
+
+                NewsAndAnnouncements.UpdateNewsAndAnnouncementsEntity(_dbContext, newsAndAnnouncementsID, title, Convert.ToDateTime(date), link,
+                    UserContext.GetUserFromHttpContext(_dbContext, HttpContext).UserID, fileResourceID);
             }
+            return Ok();
+        }
+
+        [HttpDelete("news-and-announcements/{newsAndAnnouncementsID}/delete")]
+        public ActionResult DeleteNewsAndAnnouncementsEntity([FromRoute] int newsAndAnnouncementsID)
+        {
+            var newsAndAnnouncementsDto =
+                _dbContext.NewsAndAnnouncements.SingleOrDefault(x =>
+                    x.NewsAndAnnouncementsID == newsAndAnnouncementsID);
+            if (newsAndAnnouncementsDto == null)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            NewsAndAnnouncements.Delete(_dbContext, newsAndAnnouncementsID);
             return Ok();
         }
     }
