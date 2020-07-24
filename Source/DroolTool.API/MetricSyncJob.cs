e01d2870f83a5bd799f8cf4b88f77d4dfd7ed10c
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-07-24 14:50:31 -0700

    Get latest metric file with some string parsing instead of a fully-specified configured filename

--- Source/DroolTool.API/MetricSyncJob.cs
+++ Source/DroolTool.API/MetricSyncJob.cs
@@ -18,7 +18,7 @@ namespace DroolTool.API
     public class
         MetricSyncJob : ScheduledBackgroundJobBase<MetricSyncJob>
     {
-        public const string JobName = "Beehive Sync";
+        public const string JobName = "Metric Sync";
         private readonly DroolToolConfiguration _droolToolConfiguration;
 
         public MetricSyncJob(IWebHostEnvironment webHostEnvironment, ILogger<MetricSyncJob> logger, DroolToolDbContext droolToolDbContext, IOptions<DroolToolConfiguration> droolToolConfigurationOptions) : base("Beehive Sync", logger, webHostEnvironment, droolToolDbContext)
@@ -29,24 +29,8 @@ namespace DroolTool.API
         public override List<RunEnvironment> RunEnvironments => new List<RunEnvironment> { RunEnvironment.Development, RunEnvironment.Staging, RunEnvironment.Production };
         protected override void RunJobImplementation()
         {
-            // Get the object used to communicate with the server.
-            var request = (FtpWebRequest)WebRequest.Create(_droolToolConfiguration.MetricsDatabaseFTPUrl);
-            request.Method = WebRequestMethods.Ftp.DownloadFile;
-
-            request.Credentials = new NetworkCredential(_droolToolConfiguration.MNWDFileTransferUsername,
-                _droolToolConfiguration.MNWDFileTransferPassword);
-
-            var tempFileName = Path.GetTempFileName();
-
-            using (var response = (FtpWebResponse)request.GetResponse())
-            using (var responseStream = response.GetResponseStream())
-            using (var reader = new StreamReader(responseStream ?? throw new InvalidOperationException("The ResponseStream from the FTP request was null.")))
-            using (var destination = new StreamWriter(tempFileName))
-            {
-                destination.Write(reader.ReadToEnd());
-                destination.Flush();
-            }
-
+            var tempFileName = DownloadLatestMetricFileToTempFileAndReturnTempFileName(_droolToolConfiguration.MetricsDatabaseFTPUrl, _droolToolConfiguration.MNWDFileTransferUsername, _droolToolConfiguration.MNWDFileTransferPassword);
+            
             _logger.LogInformation($"Length of downloaded file: {new FileInfo(tempFileName).Length}");
 
             var dataTable = LoadMetricsToDataTable(tempFileName);
@@ -139,6 +123,53 @@ namespace DroolTool.API
 
             s.BulkCopyTimeout = 600;
             s.WriteToServer(csvFileData);
+        }
+
+        public static string DownloadLatestMetricFileToTempFileAndReturnTempFileName(string url, string username, string password)
+        {
+            // Get the object used to communicate with the server.
+            var directoryRequest = (FtpWebRequest)WebRequest.Create(url);
+            directoryRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            directoryRequest.Credentials = new NetworkCredential(username, password);
+            var lines = new List<string>();
+            using (var response = (FtpWebResponse)directoryRequest.GetResponse())
+            {
+                using var responseStream = response.GetResponseStream();
+                var reader = new StreamReader(responseStream);
+                while (!reader.EndOfStream)
+                {
+                    lines.Add(reader.ReadLine());
+                }
+            }
+
+            var filenames = lines.Select(x => x.Split(new[] {' ', '\t'}).Last());
+
+            var latestMetricFilename = filenames.Where(x => x.Contains("Metrics")).Select(x =>
+            {
+                var split = x.Split(new[] { '.', '_' });
+                // Not trying to get political, but think it's a safe bet that we don't need to worry about year 2100+ for right now.
+                var year = 2000 + Int32.Parse(split[2]);
+                var month = Int32.Parse(split[3]);
+                var day = Int32.Parse(split[4]);
+                return new { Filename = x, Date = new DateTime(year, month, day) };
+            }).OrderByDescending(x=>x.Date).First().Filename;
+
+            var request = (FtpWebRequest)WebRequest.Create($"{url}{latestMetricFilename}");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential(username,password);
+
+            var tempFileName = Path.GetTempFileName();
+
+            using (var response = (FtpWebResponse)request.GetResponse())
+            using (var responseStream = response.GetResponseStream())
+            using (var reader = new StreamReader(responseStream ?? throw new InvalidOperationException("The ResponseStream from the FTP request was null.")))
+            using (var destination = new StreamWriter(tempFileName))
+            {
+                destination.Write(reader.ReadToEnd());
+                destination.Flush();
+            }
+
+            return tempFileName;
         }
     }
 }
