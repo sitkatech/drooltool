dbe10de42cc8567763878f8ed43c81d8d03c40bf
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-07-31 12:27:00 -0700

    * Add a .MakeValid to the geoserver view for neighborhoods * Re-do the backbone import with the 4326 column correct

--- Database/Objects/Views/dbo.vGeoServerNeighborhood.sql
+++ Database/Objects/Views/dbo.vGeoServerNeighborhood.sql
@@ -7,8 +7,8 @@ Select
 	n.OCSurveyNeighborhoodID,
 	n.OCSurveyDownstreamNeighborhoodID,
 	wa.WatershedAliasName as 'Watershed',
-	n.NeighborhoodGeometry4326 as NeighborhoodGeometry,
-	n.NeighborhoodGeometry.STArea() * 2471054 as Area
+	n.NeighborhoodGeometry4326.MakeValid() as NeighborhoodGeometry,
+	n.NeighborhoodGeometry.MakeValid().STArea() * 2471054 as Area
 from dbo.Neighborhood n
 join dbo.WatershedAlias wa on n.Watershed = wa.WatershedName
 GO
