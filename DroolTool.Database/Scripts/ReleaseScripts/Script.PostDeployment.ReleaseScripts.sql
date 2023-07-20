71f01f99bf5ecdc5ff3104014d816236756b2ec2
liz-arikawa-esa <larikawa@esassoc.com>
2023-07-20 14:56:05 -0700

    Move the tables over to DroolTool.Database

---
+++ DroolTool.Database/Scripts/ReleaseScripts/Script.PostDeployment.ReleaseScripts.sql
@@ -0,0 +1,13 @@
+/*
+Post-Deployment Script
+--------------------------------------------------------------------------------------
+This file is generated on every build, DO NOT modify.
+--------------------------------------------------------------------------------------
+*/
+
+PRINT N'DroolTool.Database - Script.PostDeployment.ReleaseScripts.sql';
+GO
+
+:r ".\0001 - Add RTE for Nitrogen Report.sql"
+GO
+
