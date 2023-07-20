7a789e904a88af8b57b041282efa055e994de854
liz-arikawa-esa <larikawa@esassoc.com>
2023-07-20 14:57:22 -0700

    remove Database folder

--- Database/ReleaseScript/0015 Add Feedback  table.sql
+++
@@ -1,8 +0,0 @@
-create table dbo.Feedback (
-	FeedbackID int not null identity(1,1) constraint PK_Feedback_FeedbackID primary key,
-	FeedbackDate datetime not null,
-	FeedbackContent varchar(max) not null,
-	FeedbackName varchar(100) null,
-	FeedbackEmail varchar(100) null,
-	FeedbackPhoneNumber varchar(20) null
-)
--- Database/ReleaseScript/0016 Add Provide Feedback to customrichtexttype and customrichtext tables.sql
+++
@@ -1,5 +0,0 @@
-insert into dbo.CustomRichTextType (CustomRichTextTypeID, CustomRichTextTypeName, CustomRichTextTypeDisplayName)
-values (4, 'ProvideFeedback', 'Provide Feedback')
-
-insert into dbo.CustomRichText (CustomRichTextTypeID, CustomRichTextContent)
-values (4, 'Provide Feedback Content')
