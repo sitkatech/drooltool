4ad26ed2f7733c2969e254494a860b89ef548061
liz-arikawa-esa <larikawa@esassoc.com>
2023-07-21 11:15:38 -0700

    Update backbone segment type lookup table

--- DroolTool.Database/Scripts/LookupTables/dbo.BackboneSegmentType.sql
+++ DroolTool.Database/Scripts/LookupTables/dbo.BackboneSegmentType.sql
@@ -1,7 +1,17 @@
-delete from BackboneSegmentType
-
-Insert into dbo.BackboneSegmentType (BackboneSegmentTypeID, BackboneSegmentTypeName, BackboneSegmentTypeDisplayName)
-values 
+MERGE INTO dbo.BackboneSegmentType AS Target
+USING (VALUES
 (1, 'Dummy', 'Dummy'),
 (2,'StormDrain', 'Storm Drain'),
 (3, 'Channel', 'Channel')
+)
+AS Source (BackboneSegmentTypeID, BackboneSegmentTypeName, BackboneSegmentTypeDisplayName)
+ON Target.BackboneSegmentTypeID = Source.BackboneSegmentTypeID
+WHEN MATCHED THEN
+UPDATE SET
+	BackboneSegmentTypeName = Source.BackboneSegmentTypeName,
+	BackboneSegmentTypeDisplayName = Source.BackboneSegmentTypeDisplayName
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (BackboneSegmentTypeID, BackboneSegmentTypeName, BackboneSegmentTypeDisplayName)
+	VALUES (BackboneSegmentTypeID, BackboneSegmentTypeName, BackboneSegmentTypeDisplayName)
+WHEN NOT MATCHED BY SOURCE THEN
+	DELETE;
