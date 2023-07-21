ff24d0c240071242c171be2f1e88aba5e07931a8
liz-arikawa-esa <larikawa@esassoc.com>
2023-07-21 10:41:40 -0700

    Remove unnecessary sql lines from tables

--- DroolTool.Database/Scripts/LookupTables/Script.PostDeployment.LookupTables.sql
+++ DroolTool.Database/Scripts/LookupTables/Script.PostDeployment.LookupTables.sql
@@ -1,11 +1,19 @@
 /*
-Post-Deployment Script
+Lookup-Table Script
 --------------------------------------------------------------------------------------
 This file is generated on every build, DO NOT modify.
 --------------------------------------------------------------------------------------
 */
 
-PRINT N'DroolTool.Database - Script.PostDeployment.ReleaseScripts.sql';
+PRINT N'DroolTool.Database - Script.PostDeployment.LookupTables.sql';
 GO
 
+:r ".\dbo.BackboneSegmentType.sql"
+GO
+:r ".\dbo.CustomRichTextType.sql"
+GO
+:r ".\dbo.FileResourceMimeType.sql"
+GO
+:r ".\dbo.Role.sql"
+GO
 
