6e280bf74e7e9debef41dd0990f752bad1dfc185
raynardlee <raynardlee@yahoo.com>
2025-01-17 13:12:43 -0800

    listening to port 8080; updating geoserver to 2.25.2

--- DroolTool.Web/Startup.cs
+++ DroolTool.Web/Startup.cs
@@ -2,7 +2,6 @@ using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -36,14 +35,7 @@ namespace DroolTool.Web
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, IHostApplicationLifetime applicationLifetime)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                var options = new RewriteOptions().AddRedirectToHttps(301, 9001);
-                app.UseRewriter(options);
-            }
-            
+        {            
             app.Use(async (context, next) =>
             {
                 if (context.Request.Path.Value == "/assets/config.json")
